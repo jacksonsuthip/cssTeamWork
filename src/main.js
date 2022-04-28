862a5df5cbc0a7fbb118c21570faef8131a475e0
antoj <jackantoaj@gmail.com>
2022-04-28 16:45:03 +0530

    All

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CssTask from "./anto/cssTask";
+import Design from "./greeshma/Design";
+import './mainStyle.css';
+import Header from "./prasanth/Header";
+import Design1 from "./sakthi/Design1";
+import FrontDesign from "./sushma/FrontDesign";
+
+function Main() {
+    return (
+        <>
+            <div className="headermain"><Header /></div>
+            <div className="design1main"><Design1 /></div>
+            <div className="csstaskmain"><CssTask /></div>
+            <div className="designmain"><Design /></div>
+            <div className="fdmain"><FrontDesign /></div>
+        </>
+    );
+}
+export default Main;

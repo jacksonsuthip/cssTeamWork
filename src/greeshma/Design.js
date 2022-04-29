4ff0a586182f2d5c116ddbdae497be992443d0f6
greeshma-ms <greeshmamelsonphilip@gmail.com>
2022-04-29 09:41:21 +0530

    greeshma-profile

---
+++ src/greeshma/Design.js
@@ -0,0 +1,172 @@
+import React from "react";
+// import { Route, Routes, Link, NavLink } from 'react-router-dom';
+import './style.css'
+function Design() {
+    return (
+        <div className="main">
+            <div className="container" >
+
+                <div className="center">
+                    <div className="basic-info">
+                        <button className="add-btn">
+                            <p className="btn-txt">+</p></button>
+                        <div className="basic-head">
+                            <h4>Basic info</h4>
+                            <h3>Company</h3>
+                        </div>
+                        <div className="content">
+                            <li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic1" style={{ background: '#57BB5C ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li>
+                            <li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic2" style={{ background: '#E33390  ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li>
+                            <li className="content-body1" >
+                                <input type="checkbox" />
+                                <div className="pro-pic3" style={{ background: '#C6BCC1  ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic4 " style={{ background: '#ECEA6B  ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic5" style={{ background: '#E74216  ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic6" style={{ background: '#DC9D8C  ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic7" style={{ background: '#9B86D6 ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic8" style={{ background: ' #A06699' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li>
+                            <li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic9" style={{ background: '#66A09F' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li><li className="content-body">
+                                <input type="checkbox" />
+                                <div className="pro-pic10" style={{ background: '#586359 ' }}>
+                                    <h6 style={{ color: 'white', fontSize: '20px', marginLeft: "9", marginTop: '6' }}>mm</h6>
+                                </div>
+                                <div className="content-txt">
+                                    <h4>Lorem Ipsum </h4>
+                                    <p className="cont-para">Lorem ipsum dolor sit </p>
+                                </div>
+                                <div className="cont-right">
+                                    <h5>Translations:</h5>
+                                </div>
+
+
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+export default Design;
